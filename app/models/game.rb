b4a941c512e8c18f836cd85821558df6138cf651
github username <hector.bale11@gmail.com>
2020-09-29 14:27:35 -0500

    methods

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,22 @@
 class Game < ActiveRecord::Base
     has_many :listings
     has_many :stores, through: :listings
+
+    def self.return_all_games_with_n(name)
+            Game.where("name LIKE ?", "%#{name}%")
+    end 
+
+
+    def self.returns_top_10_discounted_games
+      top_ten = Listing.all.sort_by do |listing|
+        listing.price / listing.game.msrp
+        end
+        count = 0
+        10.times do
+         game = top_ten[count].game
+        puts "#{game.name} is discounted at #{(top_ten[0].price / game.msrp).round(2)}% off at #{top_ten[0].store.name} for a total of #{(top_ten[0].price).round(2)}"
+         count += 1
+    end
+    end 
+        
 end
